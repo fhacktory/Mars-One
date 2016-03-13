dbda60abee2f7806ba25d2ee1a4b1b74f13e8459
TFA <tfa@gmx.fr>
2016-03-13 12:26:48 +0100

    add comments & send data back to client

--- Move module/Embedded Script/script.py
+++ Move module/Embedded Script/script.py
@@ -18,44 +18,48 @@ server_sock.bind(("",port))
 server_sock.listen(1)
 
 # Connect two large motors on output ports B and C:
-ml = LargeMotor(OUTPUT_B)
-mr = LargeMotor(OUTPUT_C)
+motor_left = LargeMotor(OUTPUT_B)
+motor_right = LargeMotor(OUTPUT_C)
 
 #Touche sensor
 ts = TouchSensor();    assert ts.connected
 
-assert ml.connected
-assert mr.connected
+assert motor_left.connected
+assert motor_right.connected
 
-ml.duty_cycle_sp=0
-mr.duty_cycle_sp=0
-ml.run_direct()
-mr.run_direct()
+motor_left.duty_cycle_sp=0
+motor_right.duty_cycle_sp=0
+motor_left.run_direct()
+motor_right.run_direct()
 
 print "Socket opened waiting for client"
 client_sock,address = server_sock.accept()
 print "Accepted connection from ",address
+client_sock.sendto("Bienvenue sur EV3",address)
+motor_right.position=0
+motor_left.position=0
+
 
 def Obs():
 	#son
 	
-	#Led erreur
+	#Red leds on
 	for light in (Leds.LEFT, Leds.RIGHT):
 		Leds.set_color(light, Leds.RED)
-    #Arret des moteurs
-	ml.stop(stop_command='brake')
-	mr.stop(stop_command='brake')
+    #motors stop
+	motor_left.stop(stop_command='brake')
+	motor_right.stop(stop_command='brake')
 	#Sound.tone([(1000, 500, 500)] * 3)
 	ev3.Sound.speak('OOps I dit it again').wait()
-    #Backward
-	ml.run_timed(duty_cycle_sp=-50, time_sp=1500)
-	mr.run_timed(duty_cycle_sp=-50, time_sp=1500)
+    #Backward for 1.5 sec
+	motor_left.run_timed(duty_cycle_sp=-50, time_sp=1500)
+	motor_right.run_timed(duty_cycle_sp=-50, time_sp=1500)
 	time.sleep(1.55)
-    #Led normal
+    #Green led on
 	for light in (Leds.LEFT, Leds.RIGHT):
 		Leds.set_color(light, Leds.GREEN)
-	ml.run_direct()
-	mr.run_direct()
+	motor_left.run_direct()
+	motor_right.run_direct()
 while 1:
 	if ts.value():
 		Obs()
@@ -63,22 +67,31 @@ while 1:
 	try :
 		data = client_sock.recv(1024)
     	#print "received [%s]" % data
+		
 		cmd=data.split(';')
 	except :
-		#Le client a fermé son socket
+		#client closed socket
 		print "Client disconnected, waiting for new one"
 		client_sock.close()
 		client_sock,address = server_sock.accept()
 		print "Accepted connection from ",address
+		client_sock.sendto("Welcome on EV3",address)
+		motor_right.position=0
+		motor_left.position=0
+
+	client_sock.sendto(str(motor_left.position)+'\n',address)
+	client_sock.sendto(str(motor_right.position)+'\n',address)
+	motor_right.position=0
+	motor_left.position=0	
 
 	try :
 		if cmd[0]=='m':
-			ml.duty_cycle_sp=int(cmd[1])
-			mr.duty_cycle_sp=int(cmd[2])
-	#Si erreur commande
+			motor_left.duty_cycle_sp=int(cmd[1])
+			motor_right.duty_cycle_sp=int(cmd[2])
+	#if cmd error
 	except:
 		print "erreur" 
-    	#server_sock.send("Error : CMD_ERROR")    
+    	    
 
 
 server_sock.close()
--- Move module/Input Script/roverControler.py
+++ Move module/Input Script/roverControler.py
@@ -1,12 +1,12 @@
 from sys import platform as _platform
-import time, pygame
+import time, pygame, socket, errno
 from pygame.locals import *
 
 #import bluetooth for mac or linux
 if _platform == 'darwin':
-	import lightblue
+    import lightblue
 elif _platform == 'linux2':
-	import bluetooth
+    import bluetooth
 
 #bluetooth constantes
 bd_addr = "00:17:EC:03:19:C6"
@@ -16,9 +16,9 @@ isRunning = True
 
 #init socket
 if _platform == 'darwin':
-	sock=lightblue.socket()
+    sock=lightblue.socket()
 elif _platform == 'linux2':
-	sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+    sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
 
 #init pygame
 pygame.init()
@@ -28,62 +28,69 @@ pygame.mouse.set_visible(0)
 
 #connect socket
 try :
-	sock.connect((bd_addr, port))
-	isConnect = True
+    sock.connect((bd_addr, port))
+    sock.setblocking(0)
+    isConnect = True
 except :
-	# Cannot connect the rover
-	print "Impossible to connect the Rover. Restart script"
-	isConnect = False
+    # Cannot connect the rover
+    print "Impossible to connect the Rover. Restart script"
+    isConnect = False
 
 print 'LETS GO !'
 
 while isRunning & isConnect:
-	left = 0
-	right = 0
-	
-	pygame.event.pump()
-	keys = pygame.key.get_pressed()
-	print keys
 
-	if keys[K_p]:
-		isRunning = False
-	elif keys[K_z] | keys[K_s] | keys[K_UP] | keys[K_DOWN]:
-	    left = 100
-	    right = 100
-	    if keys[K_q] | keys[K_LEFT]:
-	    	left -= 50
-	    if keys[K_d] | keys[K_RIGHT]:
-	    	right -= 50
-	    if keys[K_s] | keys[K_DOWN]:
-	    	left *= -1
-	    	right *= -1
-	elif keys[K_q] & keys[K_d]:
-		left = 0
-		right = 0
-	elif keys[K_q] | keys[K_LEFT]:
-		left = -100
-		right = 100
-	elif keys[K_d] | keys[K_RIGHT]:
-		left = 100
-		right = -100
+    try:
+        data = sock.recv(1024)
+        print data
+    except :
+        print 'No data available'
+
+    left = 0
+    right = 0
+    
+    pygame.event.pump()
+    keys = pygame.key.get_pressed()
+
+    if keys[K_p]:
+        isRunning = False
+    elif keys[K_z] | keys[K_s] | keys[K_UP] | keys[K_DOWN]:
+        left = 100
+        right = 100
+        if keys[K_q] | keys[K_LEFT]:
+            left -= 50
+        if keys[K_d] | keys[K_RIGHT]:
+            right -= 50
+        if keys[K_s] | keys[K_DOWN]:
+            left *= -1
+            right *= -1
+    elif keys[K_q] & keys[K_d]:
+        left = 0
+        right = 0
+    elif keys[K_q] | keys[K_LEFT]:
+        left = -100
+        right = 100
+    elif keys[K_d] | keys[K_RIGHT]:
+        left = 100
+        right = -100
 
 
-	msg = 'm' +  ';' + str(left) + ';' + str(right)	
-	try :
-		sock.send(msg)
-	except :
-		print "Connection lost"
-		isConnect = False
-	print msg
+    msg = 'm' +  ';' + str(left) + ';' + str(right)    
+    try :
+        sock.send(msg)
+    except :
+        print "Connection lost"
+        isConnect = False
+    #print msg
 
-	if not isRunning:
-		try :
-			sock.send('q')
-		except :
-			print "Connection lost"
-			isConnect = False
+    if not isRunning:
+        try :
+            sock.send('q')
+        except :
+            print "Connection lost"
+            isConnect = False
 
-	# Loop frequency
-	time.sleep(0.1)
+    # Loop frequency
+    time.sleep(0.1)
 
 sock.close()
